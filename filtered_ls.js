68d22cc9e7c30fa7ccf23ac6ad93240012d84682
Brian <iamamthwiz@gmail.com>
2014-07-05 10:24:30 -0700

    Update solution

--- filtered_ls.js
+++ filtered_ls.js
@@ -7,7 +7,7 @@ var fileExtension = process.argv[3];
 fs.readdir(fileDirectory, function readdirCallback(err, files) {
   var results = [];
   files.forEach(function filesForEach(file) {
-    if (path.extname(file).indexOf(fileExtension) !== -1) {
+    if (path.extname(file).indexOf("." + fileExtension) !== -1) {
       console.log(file);
     }
   });
